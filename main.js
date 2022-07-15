e3ce5437616a58eb2d796a9fcb0c5cf24558c10e
Kidd Kaine <79826408+kiddkaine@users.noreply.github.com>
2022-07-15 15:48:44 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,73 @@
-const mainPromise = new Promise ((resolve, reject) => {
-	console.log('--- Подключение к базе данных ---');
-	setTimeout(() =>{
-		const connectDataBase = false;
-		if (connectDataBase) {
-			let message = '*+ Подключение прошло успешно! +*';
+const databasePromise = new Promise ((resolve, reject) => {
+
+	console.log('----- База данных -----');
+
+	setTimeout(() => {
+		const response = true;
+
+		if (response) {
+			let message = '*** Подключение к базе данных прошло успешно! ***';
 			resolve(message);
 		} else {
-			let message = '*+ Ошибка подключения к базе данных! +*';
+			let message = '*** Ошибка при подключении к базе данных ***';
 			reject(message);
 		}
 	}, 1000);
+	
+});
+
+databasePromise.then((data) => {
+
+	console.log(data);
+
+	return new Promise ((resolve, reject) => {
+		setTimeout(() => {
+			const response = true;
+
+			if (response) {
+				let message = '%+ Вы авторизованы! +%';
+				resolve(message);
+			} else {
+				let message = '%+ Неверный логин или пароль! +%';
+				reject(message);
+			}
+		}, 1000);
+	});
+
+}).then((data) => {
+
+	console.log(data);
+
+	return new Promise ((resolve, reject) => {
+		setTimeout(() => {
+			const response = true;
+
+			if (response) {
+				let message = '!? Доступ разрешён! ?!';
+				resolve(message);
+			} else {
+				let message = '!? Ваш аккаунт заблокирован! ?!';
+				reject(message);
+			}
+		}, 1000);
+	});
+
+}).then((data) => {
+
+	console.log(data);
+
+	return new Promise ((resolve, reject) => {
+		setTimeout(() => {
+			const response = true;
+
+			if (response) {
+				let message = '.- Изменения прошли успешно! -.';
+				resolve(message);
+			} else {
+				let message = '.- Ошибка редактирования данных! -.';
+				reject(message);
+			}
+		}, 1000);
+	});
+
 }).then((data) => console.log(data)).catch((data) => console.log(data));
